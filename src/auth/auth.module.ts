73cab0a879ebe72576c291f40a9bc15ab574ec29
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-05-19 19:47:57 +0330

    renamed "RolesGuard" to "AuthGuard" and changed its place

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,10 @@ import { UsersModule } from 'src/users/users.module';
 import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
 import { ProfileModule } from 'src/profile/profile.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from 'src/users/schemas/user.schema';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -15,9 +19,16 @@ import { ProfileModule } from 'src/profile/profile.module';
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '2h' },
     }),
+    MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
